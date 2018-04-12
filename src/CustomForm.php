90ba9086beebb13cf357c801ca8b647c315ade22
Grischuk Sasha <grischuk.alexandr@gmail.com>
2018-04-12 13:08:16 +0300

    add FormTest

--- src/CustomForm.php
+++ src/CustomForm.php
@@ -1,15 +1,10 @@
-# form-validator
-
-## Example CustomForm.php
-```
 <?php
-
-use FormValidator\Form;
+namespace FormValidator;
 
 class CustomForm extends Form
 {
     protected $attributes = [
-        'field1' => null,
+        'field1' => 1,
         'field2' => null,
         'field3' => null,
         'field4' => null,
@@ -40,45 +35,12 @@ class CustomForm extends Form
 
             ['field3', self::RULE_REQUIRED],
             ['field5', self::RULE_EMAIL],
-            
+
             ['field6', self::RULE_NUMERIC],
-            
+
             ['field6', self::RULE_INTEGER, 'min' => 10, 'max' => 1000],
-            
+
             ['field7', self::RULE_DEFAULT, 'value' => 'myDefaultValue'],
         ];
     }
-
-    public function validateName($name)
-    {
-        preg_match('/^[\p{Latin}\s\.\-[A-Za-z]+$/', $name, $matches);
-
-        if ($matches === [])
-            return false;
-
-        return true;
-    }
 }
-```
-
-### Now you can validate your data
-```
-$form = new CustomForm([
-       'field1' => 'my data',
-       'field2' => 'my data',
-       'field3' => 'my data',
-       'field4' => 'my data',
-       'field5' => 'my data',
-       'field6' => 'my data',
-       'field7' => 'my data',
-   ]);
-
-if ($form->validate()) {
-    // success action
-    $attrs = $form->getAttributes();
-
-} else {
-    // fail action
-    $errors = $form->getErrors();
-}
-```
